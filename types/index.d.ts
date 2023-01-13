85d7cfd21f124b77dd5e5eeb114d88eebbc8c186
marmotmarsh <marmotmarsh74@gmail.com>
2023-01-13 14:52:09 -0800

    differentiate between new and old proposal

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,6 +1,10 @@
-export interface IProposal {
+export interface INewProposal {
   title: string;
   description: string;
   name: string;
   email: string;
+}
+
+export interface IProposal extends INewProposal {
+  id: string;
 }
