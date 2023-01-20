f3f1e50dc5bf3c9db6fd5ce0f105fd49e5b13a42
marmotmarsh <marmotmarsh74@gmail.com>
2023-01-20 09:32:28 -0800

    added some new types

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,10 +1,39 @@
-export interface INewProposal {
+export interface NewProposal {
   title: string;
   description: string;
+  email?: string;
+  userId?: string;
+  userName?: string;
+}
+
+export interface Proposal extends NewProposal {
+  id: string;
+  createdAt: string;
+  updatedAt: string;
+}
+
+export interface PublicUser {
   name: string;
   email: string;
 }
 
-export interface IProposal extends INewProposal {
+export interface User extends PublicUser {
   id: string;
+  createdAt: string;
+  updatedAt: string;
+  isPremium: boolean;
+}
+
+export enum ThumbEnum {
+  Yes = 'Yes',
+  No = 'No',
+  Sideways = 'Sideways',
+  NoReply = 'No Reply',
+}
+
+export interface ProposalResponse {
+  id: string;
+  proposalId: string;
+  thumb: ThumbEnum;
+  comment: string;
 }
